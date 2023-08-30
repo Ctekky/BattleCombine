973ac99a8bd53c5daba1cc6e87e5e52fd8970faa
Kirill Voronov <70638098+Luciolla@users.noreply.github.com>
2023-08-30 16:31:08 +1000

    FieldRework
    add full FIeld generate;

--- BattleCombine/Assets/Scripts/Field/CreateField.cs
+++ BattleCombine/Assets/Scripts/Field/CreateField.cs
@@ -1,28 +1,72 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Scripts
 {
     public class CreateField : MonoBehaviour
     {
+        [Header("FieldSize")] 
+        [SerializeField] private FieldSize sizeType;
+        
+        [Header("FieldPrefabs List")] 
+        [SerializeField] private List<GameObject> fields;
+
+        [Header("Tile prefab")]
         [SerializeField] private GameObject tile;
-        [SerializeField] private Transform field;
-        [SerializeField, Range(4, 21)] private int fieldSize;
+        
+        private List<GameObject> tileList;
+        private GameObject currentField;
+        private Transform fieldParent;
+        private int fieldSize;
+        
 
         private void Start()
         {
-            Debug.Log("Start");
-            Create();
+            tileList = new List<GameObject>();
+            ChangeFieldSize();
+            CreateNewField();
         }
 
-        private void Create()
+        private void CreateNewField()
+        {
+            var newField = new FieldCreateFactory(currentField);
+       
+            var thisField = newField.Create(gameObject.transform);
+            fieldParent = thisField.transform;
+            
+            AddTileToField();
+        }
+
+        private void AddTileToField()
         {
             var newTile = new FieldCreateFactory(tile);
-            Debug.Log(newTile);
             
             for (var i = 0; i < fieldSize; i++)
             {
-                Debug.Log("Create " + i);
-                var gameObject = newTile.Create(field);
+                var currentTile = newTile.Create(fieldParent);
+                tileList.Add(currentTile);
+            }
+        }
+
+        private void ChangeFieldSize()
+        {
+            switch (sizeType)
+            {
+                case FieldSize.Small:
+                    fieldSize = 36;
+                    currentField = fields[0];
+                    break;
+                case FieldSize.Medium:
+                    fieldSize = 49;
+                    currentField = fields[1];
+                    break;
+                case FieldSize.Large:
+                    fieldSize = 64;
+                    currentField = fields[2];
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
     }
