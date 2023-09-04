bdb9af135d9a5c7043ef6cd17126daaa27141858
Kirill Voronov <70638098+Luciolla@users.noreply.github.com>
2023-09-04 15:41:03 +1000

    FixScaler & configure field

--- BattleCombine/Assets/Resources/_Scripts/Field/CreateField.cs
+++ BattleCombine/Assets/Resources/_Scripts/Field/CreateField.cs
@@ -1,20 +1,22 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Scripts
 {
     public class CreateField : MonoBehaviour
     {
+        [Header("Scale or not on start")] 
+        [SerializeField] private bool makeScale;
+        [Header("MainField")] 
+        [SerializeField] private GameObject field;
         [Header("FieldSize")] 
         [SerializeField] private FieldSize sizeType;
         [Header("TileParent")] 
         [SerializeField] private GameObject tileParent;
         [Header("Tile prefab")] 
         [SerializeField] private GameObject tile;
-        [FormerlySerializedAs("_offset")]
-        [Header("Offsets & scales (test values)")]
+        [Header("Offsets & scales (test values)")] 
         [SerializeField] private float _tileOffset = 1.1f;
         [SerializeField] private float smallFieldScale = 1.45f;
         [SerializeField] private float mediumFieldScale = 1.2f;
@@ -23,6 +25,8 @@ namespace Scripts
         private List<GameObject> _tileList;
         private Transform _fieldParent;
         private int _fieldSize;
+
+        public List<GameObject> GetTileList => _tileList;
 
         private void Start()
         {
@@ -43,8 +47,10 @@ namespace Scripts
 
             AddTileToField();
             ModifyTitleSize();
+            if (!makeScale) return;
+            ScaleMainField();
         }
-        
+
         private void AddTileToField()
         {
             var newTile = new FieldCreateFactory(tile);
@@ -62,7 +68,7 @@ namespace Scripts
                 }
             }
         }
-        
+
         private void ModifyTitleSize()
         {
             tileParent.transform.localScale = _fieldSize switch
@@ -72,6 +78,24 @@ namespace Scripts
                 8 => new Vector3(largeFieldScale, 0, largeFieldScale),
                 _ => tileParent.transform.localScale
             };
+        }
+
+        private void ScaleMainField()
+        {
+            //test scaler (not complete)
+            var padding = .5f;
+
+            var mainCamera = Camera.main;
+            var halfHeight = mainCamera.orthographic ? mainCamera.orthographicSize : 0;
+            var halfWidth = mainCamera.aspect * halfHeight;
+
+            var objectWidth = field.GetComponent<Renderer>().bounds.size.x;
+            var objectHeight = field.GetComponent<Renderer>().bounds.size.y;
+
+            var scaleX = (halfWidth * 2f - padding * 2) / objectWidth;
+            var scaleY = (halfHeight * 2f - padding * 2) / objectHeight;
+
+            field.transform.localScale = new Vector3(scaleX, scaleY, 1);
         }
     }
 }
