dc6c19d8acafc4d236e09756f8be0260202687a9
Dmitriy Sergeev <ctekky@gmail.com>
2023-09-09 17:09:06 +0400

    gamestatemachine

---
+++ BattleCombine/Assets/_Scripts/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using BattleCombine.Interfaces;
+using UnityEngine;
+
+namespace BattleCombine.Infrastructure
+{
+    public class GameBootstrapper : MonoBehaviour, ICoroutineRunner
+    {
+        private Game _game;
+
+        private void Awake()
+        {
+            _game = new Game(this);
+            _game.StateMachine.Enter<BootStrapState>();
+            DontDestroyOnLoad(this);
+        }
+    }
+}
