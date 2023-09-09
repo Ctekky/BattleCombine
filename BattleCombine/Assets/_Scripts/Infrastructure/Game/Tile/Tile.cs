a9b67804837dcc94a3c4bb86d3139068d6ba434b
Dmitriy Sergeev <ctekky@gmail.com>
2023-09-09 18:36:41 +0400

    added types to tile

--- BattleCombine/Assets/_Scripts/Infrastructure/Game/Tile/Tile.cs
+++ BattleCombine/Assets/_Scripts/Infrastructure/Game/Tile/Tile.cs
@@ -1,8 +1,9 @@
 using BattleCombine.Interfaces;
 using System;
 using System.Collections.Generic;
+using BattleCombine.ScriptableObjects;
+using TMPro;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace _Scripts
 {
@@ -14,17 +15,24 @@ namespace _Scripts
         [SerializeField] private List<GameObject> _tilesForChoosing;
         [SerializeField] private List<GameObject> _tilesNearThisTile;
 
+        //TODO: назначать тайлу тип (от типа зависит иконка) и модификатор (от него зависит число)
+        [SerializeField] private TileType _tileType;
+        [SerializeField, Range(-100, 100)] private float _tileModifier;
+        [SerializeField] private TextMeshPro _text;
+
         public StateMachine StateMachine;
         public AvailableForSelectionState AvailableForSelectionState;
         public ChosenState ChosenState;
         public DisabledState DisabledState;
         public EnabledState EnabledState;
         public FinalChoiceState FinalChoiceState;
-        public List<GameObject> TilesForChoosing 
+
+        public List<GameObject> TilesForChoosing
         {
             get => _tilesForChoosing;
-            set => _tilesForChoosing = value; 
+            set => _tilesForChoosing = value;
         }
+
         public List<GameObject> TilesNearThisTile
         {
             get => _tilesNearThisTile;
@@ -54,30 +62,56 @@ namespace _Scripts
             }
 
             CheckTilesStateNearThisTile(this);
+            //TODO: помен€ть алгоритм 
+            _tileModifier = 5;
+            SetupTile();
         }
+
+        public void ChangeTileType(TileType type)
+        {
+            _tileType = type;
+        }
+
+        public void ChangeTileModifier(float modifier)
+        {
+            _tileModifier = modifier;
+        }
+
+        private void SetupTile()
+        {
+            _text.text = _tileModifier.ToString();
+            spriteRenderer.sprite = _tileType.sprite;
+        }
+
         private void Update()
         {
-            _stateName = StateMachine.CurrentState.ToString(); //¬ывожу им€ класса дл€ понимани€ в каком state находитс€ tile
+            _stateName =
+                StateMachine.CurrentState.ToString(); //¬ывожу им€ класса дл€ понимани€ в каком state находитс€ tile
         }
+
         public void ChangeClolor(Color color)
         {
             spriteRenderer.color = color;
         }
+
         public void Touch()
         {
             StateMachine.CurrentState.Input();
             StateMachine.CurrentState.LogicUpdate();
         }
-        public void FindTileForChoosing(Tile tile, List<GameObject> list) // ћетод нахождени€ соседних tile дл€ изменени€ их состо€ни€
+
+        public void
+            FindTileForChoosing(Tile tile,
+                List<GameObject> list) // ћетод нахождени€ соседних tile дл€ изменени€ их состо€ни€
         {
             Vector2 tilePosition = tile.transform.position;
             float tileOverlapScaleX = gameObject.transform.localScale.x + range;
             float tileOverlapScaleY = gameObject.transform.localScale.y + range;
             Vector2 tileScale = new Vector2(tileOverlapScaleX, tileOverlapScaleY);
             Collider2D[] tileColliderForChoosing = Physics2D.OverlapBoxAll(tilePosition, tileScale, 45f, TileLayerMask);
-            
+
             Collider2D thisTileCollider = this.gameObject.GetComponent<Collider2D>();
-            foreach(var colliderTile in tileColliderForChoosing)
+            foreach (var colliderTile in tileColliderForChoosing)
             {
                 if (colliderTile == thisTileCollider)
                 {
@@ -90,15 +124,17 @@ namespace _Scripts
                 }
             }
         }
+
         public void ClearTheTilesArray() // ћетод очистки массива найденых tile 
         {
             TilesForChoosing.Clear();
         }
 
-        public void CheckTilesStateNearThisTile(Tile tile)// ћетод нахождени€ соседних tile
+        public void CheckTilesStateNearThisTile(Tile tile) // ћетод нахождени€ соседних tile
         {
             FindTileForChoosing(tile, TilesNearThisTile);
         }
+
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
@@ -109,9 +145,8 @@ namespace _Scripts
             Gizmos.DrawWireCube(position, tileScale);
 
             Gizmos.color = Color.green;
-            float rangeOverlap = (float)((Math.Sqrt(Math.Pow(tileScaleX, 2) + Math.Pow(tileScaleY, 2)))/2);
+            float rangeOverlap = (float)((Math.Sqrt(Math.Pow(tileScaleX, 2) + Math.Pow(tileScaleY, 2))) / 2);
             Gizmos.DrawWireSphere(position, rangeOverlap);
         }
     }
 }
-
