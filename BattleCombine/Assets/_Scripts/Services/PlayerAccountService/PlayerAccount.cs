df8b2d51c9853e154ccf5c039202ef78aaa57135
Dmitriy Sergeev <ctekky@gmail.com>
2023-12-01 12:05:21 +0400

    fix namespace error

--- BattleCombine/Assets/_Scripts/Services/PlayerAccountService/PlayerAccount.cs
+++ BattleCombine/Assets/_Scripts/Services/PlayerAccountService/PlayerAccount.cs
@@ -1,27 +1,33 @@
-public class PlayerAccount
+namespace BattleCombine.Data
 {
-    public string PlayerID;
-    public string PlayerName;
-    public int Exp;
-    public int Gold;
-    public int CurrentScore;
-    public int MaxScore;
-    public PlayerAccount(string playerID, string playerName, int exp, int gold, int currentScore, int maxScore)
+    public class PlayerAccount
     {
-        PlayerID = playerID;
-        PlayerName = playerName;
-        Exp = exp;
-        Gold = gold;
-        CurrentScore = currentScore;
-        MaxScore = maxScore;
+        public string PlayerID;
+        public string PlayerName;
+        public int Exp;
+        public int Gold;
+        public int CurrentScore;
+        public int MaxScore;
+
+        public PlayerAccount(string playerID, string playerName, int exp, int gold, int currentScore, int maxScore)
+        {
+            PlayerID = playerID;
+            PlayerName = playerName;
+            Exp = exp;
+            Gold = gold;
+            CurrentScore = currentScore;
+            MaxScore = maxScore;
+        }
+
+        public PlayerAccount()
+        {
+            PlayerID = "0000";
+            PlayerName = "TestName";
+            Exp = 0;
+            Gold = 0;
+            CurrentScore = 0;
+            MaxScore = 0;
+        }
     }
-    public PlayerAccount()
-    {
-        PlayerID = "0000";
-        PlayerName = "TestName";
-        Exp = 0;
-        Gold = 0;
-        CurrentScore = 0;
-        MaxScore = 0;
-    }
+    
 }
