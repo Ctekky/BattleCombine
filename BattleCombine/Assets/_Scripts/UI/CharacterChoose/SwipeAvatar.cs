69c1cffaf64b427dfdd0d01c5d9648dddc2c0c4f
Kirill Voronov <70638098+Luciolla@users.noreply.github.com>
2024-03-13 16:49:17 +1000

    AvatarsUpdate

--- BattleCombine/Assets/_Scripts/UI/CharacterChoose/SwipeAvatar.cs
+++ BattleCombine/Assets/_Scripts/UI/CharacterChoose/SwipeAvatar.cs
@@ -7,52 +7,58 @@ namespace _Scripts.UI.CharacterChoose
 {
 	public class SwipeAvatar : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 	{
-		//[SerializeField] private SwipeProvider provider;
+		[SerializeField] private SwipeAvatarProvider _provider;
 
-		[SerializeField] private Transform currentAvatar;
-
-		private Vector3 _initialPosition;
-		private float _distanceMoved;
-		private bool _swipeLeft;
+		private Transform currentAvatar;
+		private Vector3 initialPosition;
+		
+		private float distanceMoved;
+		private bool swipeLeft;
 
 		private void OnEnable()
 		{
 			transform.localPosition = Vector3.zero;
 			transform.localEulerAngles = Vector3.zero;
+
+			currentAvatar = this.gameObject.transform;
+		}
+
+		public void OnBeginDrag(PointerEventData eventData)
+		{
+			initialPosition = transform.localPosition;
 		}
 
 		public void OnDrag(PointerEventData eventData)
 		{
 			transform.localPosition = new Vector2(transform.localPosition.x + eventData.delta.x, transform.localPosition.y);
 
-			if(transform.localPosition.x - _initialPosition.x > 0)
+			if(transform.localPosition.x - initialPosition.x > 0)
 			{
 				transform.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(0, 0,
-					(_initialPosition.x + transform.localPosition.x) / (Screen.width / 2)));
+					(initialPosition.x + transform.localPosition.x) / (Screen.width / 2)));
+
+				swipeLeft = true;
 			}
 			else
 			{
 				transform.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(0, 0,
-					(_initialPosition.x - transform.localPosition.x) / (Screen.width / 2)));
+					(initialPosition.x - transform.localPosition.x) / (Screen.width / 2)));
+				
+				swipeLeft = false;
 			}
-		}
-
-		public void OnBeginDrag(PointerEventData eventData)
-		{
-			_initialPosition = transform.localPosition;
 		}
 
 		public void OnEndDrag(PointerEventData eventData)
 		{
-			_distanceMoved = Mathf.Abs(transform.localPosition.x - _initialPosition.x);
-			if(_distanceMoved < 0.2 * Screen.width)
+			distanceMoved = Mathf.Abs(transform.localPosition.x - initialPosition.x);
+			if(distanceMoved < 0.2 * Screen.width)
 			{
-				transform.localPosition = _initialPosition;
+				transform.localPosition = initialPosition;
 				transform.localEulerAngles = Vector3.zero;
 			}
 			else
 			{
-				_swipeLeft = !(transform.localPosition.x > _initialPosition.x);
+				swipeLeft = !(transform.localPosition.x > initialPosition.x);
 				StartCoroutine(MovedCard());
 			}
 		}
@@ -66,14 +72,14 @@ namespace _Scripts.UI.CharacterChoose
 			while (time < colorFadeDuration)
 			{
 				time += Time.deltaTime;
+				var position = currentAvatar.localPosition;
 
-				var newPositionX = Mathf.SmoothStep(currentAvatar.localPosition.x, currentAvatar.localPosition.x + (_swipeLeft ? -1 : 1) * Screen.width,
+				var newPositionX = Mathf.SmoothStep(position.x, position.x + (swipeLeft ? -1 : 1) * Screen.width,
 					moveSpeed * Time.deltaTime);
 
-				currentAvatar.localPosition = new Vector3(newPositionX, currentAvatar.localPosition.y, currentAvatar.localPosition.z);
+				currentAvatar.localPosition = new Vector3(newPositionX, position.y, position.z);
 
-				var image = currentAvatar.GetComponent<Image>();
-				if(image != null)
+				if(currentAvatar.TryGetComponent<Image>(out var image))
 				{
 					image.color = new Color(image.color.r, image.color.g, image.color.b, Mathf.Lerp(1, 0, time / colorFadeDuration));
 				}
@@ -81,10 +87,16 @@ namespace _Scripts.UI.CharacterChoose
 				yield return null;
 			}
 
-			// if (_swipeLeft)
-			//     provider.SwipeLeft();
-			// else
-			//     provider.SwipeRight();
+			if (swipeLeft)
+			    _provider.AvatarMoveLeft();
+			else
+			    _provider.AvatarMoveRight();
+
+			currentAvatar.localPosition = initialPosition;
+			if(currentAvatar.TryGetComponent<Image>(out var images))
+			{
+				images.color = new Color(images.color.r, images.color.g, images.color.b, 1);
+			}
 		}
 	}
 }
