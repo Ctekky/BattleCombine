2561b244ec119099b964164a5d9e7d71c9796979
Kirill Voronov <70638098+Luciolla@users.noreply.github.com>
2024-03-19 17:56:38 +1000

    FixUi (size/scale/font)

--- BattleCombine/Assets/_Scripts/Temp/PlayerUI.cs
+++ BattleCombine/Assets/_Scripts/Temp/PlayerUI.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using BattleCombine.Gameplay;
 using BattleCombine.Animations;
 using BattleCombine.Services;
 using BattleCombine.UI;
 using TMPro;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -14,188 +12,166 @@ using UnityEngine.UI;
 
 namespace _Scripts.Temp
 {
-    public class PlayerUI : MonoBehaviour
-    {
-        [SerializeField] private TMP_Text attackText;
-        [SerializeField] private TMP_Text healthText;
-        [SerializeField] private TMP_Text playerLevelText;
-        [SerializeField] private Slider playerLevelSlider;
-        [SerializeField] private Image shieldSprite;
-        [SerializeField] private GameObject avatarGameObject;
-        [SerializeField] private GameObject speedArea;
-        [SerializeField] private GameObject speedPrefab;
-        [SerializeField] private GameObject speedPrefabBonus;
-        [SerializeField] private List<GameObject> createdSpeedObjectList;
-        [SerializeField] private Animator enemyRerollAnimator;
-        [SerializeField] private AnimationTriggerToEventRelay animToTrigger;
+	public class PlayerUI : MonoBehaviour
+	{
+		public event Action onEndRerollTrigger;
 
-        [Header("BoostTimer")] [SerializeField]
-        private Image _heartImg;
+		[SerializeField] private TMP_Text attackText;
+		[SerializeField] private TMP_Text healthText;
+		[SerializeField] private TMP_Text playerLevelText;
+		[SerializeField] private Slider playerLevelSlider;
+		[SerializeField] private Image shieldSprite;
+		[SerializeField] private GameObject speedArea;
+		[SerializeField] private GameObject speedPrefab;
+		[SerializeField] private GameObject speedPrefabBonus;
+		[SerializeField] private List<GameObject> createdSpeedObjectList;
+		[SerializeField] private Animator enemyRerollAnimator;
+		[SerializeField] private AnimationTriggerToEventRelay animToTrigger;
 
-        [SerializeField] private Image _swordsImg;
-        [SerializeField] private Image _heartsBackImg;
-        [SerializeField] private Image _swordsBackImg;
-        [SerializeField] private List<Sprite> _cooldownSprites;
-        [SerializeField] private GameObject _healthCooldownObj;
-        [SerializeField] private GameObject _attackCooldownObj;
-        [SerializeField] private TMP_Text _healthTimerText;
-        [SerializeField] private TMP_Text _attackTimerText;
+		[Header("BoostTimer")] [SerializeField]
+		private Image _heartImg;
+
+		[SerializeField] private Image _swordsImg;
+		[SerializeField] private Image _heartsBackImg;
+		[SerializeField] private Image _swordsBackImg;
+		[SerializeField] private List<Sprite> _cooldownSprites;
+		[SerializeField] private GameObject _healthCooldownObj;
+		[SerializeField] private GameObject _attackCooldownObj;
+		[SerializeField] private TMP_Text _healthTimerText;
+		[SerializeField] private TMP_Text _attackTimerText;
+
+		[Header("Avatars")]
+		[SerializeField] private GameObject avatarGameObject;
 
 
-        private bool _isShielded;
-        private bool _isPlayerBoostCooldownOn;
+		private bool _isShielded;
 
-        private void OnDisable()
-        {
-            DeleteAllSpeedObject();
-        }
+		private bool _isPlayerBoostCooldownOn;
 
-        public List<GameObject> GetCreatedSpeedObjectList
-        {
-            get => createdSpeedObjectList;
-        }
+		private AnimationService _animationService;
 
-        private AnimationService _animationService;
+		public List<GameObject> GetCreatedSpeedObjectList
+		{
+			get => createdSpeedObjectList;
+		}
 
-        public event Action onEndRerollTrigger; 
+		private void OnEnable()
+		{
+			if(animToTrigger != null) animToTrigger.onRerollTrigger += EndRerollAnimation;
+		}
 
-        private void OnEnable()
-        {
-            if (animToTrigger != null) animToTrigger.onRerollTrigger += EndRerollAnimation;
+		public void UpdateLevelSlider(float value)
+		{
+			playerLevelSlider.value = value;
+		}
 
-        }
+		public void UpdatePlayerLevel(int value)
+		{
+			playerLevelText.text = value.ToString();
+		}
 
-        public void UpdateLevelSlider(float value)
-        {
-            playerLevelSlider.value = value;
-        }
+		public void SetupAnimationService(AnimationService animationService)
+		{
+			_animationService = animationService;
+		}
 
-        public void UpdatePlayerLevel(int value)
-        {
-            playerLevelText.text = value.ToString();
-        }
+		public void PlayRerollAnimation(string animName)
+		{
+			_animationService.PlayAnim(animName, enemyRerollAnimator);
+		}
 
-        public void SetupAnimationService(AnimationService animationService)
-        {
-            _animationService = animationService;
-        }
+		private void EndRerollAnimation(string animName)
+		{
+			_animationService.StopAnim(animName, enemyRerollAnimator);
+			onEndRerollTrigger?.Invoke();
+		}
 
-        public void PlayRerollAnimation(string animName)
-        {
-            _animationService.PlayAnim(animName, enemyRerollAnimator);
-        }
+		private void SetupStat(TMP_Text text, string value)
+		{
+			text.text = value;
+		}
 
-        private void EndRerollAnimation(string animName)
-        {
-            _animationService.StopAnim(animName, enemyRerollAnimator);
-            onEndRerollTrigger?.Invoke();
-        }
+		public void SetUpAllStats(string attackValue, string healthValue, bool isShielded)
+		{
+			SetupStat(attackText, attackValue);
+			SetupStat(healthText, healthValue);
+			SetShield(isShielded);
+			ChangeShieldSprite();
+		}
 
-        public void ChangeImageInCooldown(int value)
-        {
-            _isPlayerBoostCooldownOn = !_isPlayerBoostCooldownOn;
+		private void SetShield(bool value)
+		{
+			_isShielded = value;
+			ChangeShieldSprite();
+		}
 
-            _healthCooldownObj.SetActive(_isPlayerBoostCooldownOn);
-            _attackCooldownObj.SetActive(_isPlayerBoostCooldownOn);
+		private void ChangeShieldSprite()
+		{
+			shieldSprite.enabled = _isShielded;
+		}
 
-            if (_isPlayerBoostCooldownOn)
-            {
-                _heartImg.sprite = _cooldownSprites[1];
-                _heartsBackImg.sprite = _cooldownSprites[5];
-                _healthTimerText.text = value + "h";
+		public void SetupAvatar(Sprite enableState, Sprite disableState)
+		{
+			var changeSpriteScript = avatarGameObject.GetComponent<UISpriteImageStateChange>();
+			changeSpriteScript.SetupSprites(enableState, disableState);
+		}
 
-                _swordsImg.sprite = _cooldownSprites[3];
-                _swordsBackImg.sprite = _cooldownSprites[5];
-                _attackTimerText.text = value + "h";
-            }
-            else
-            {
-                _heartImg.sprite = _cooldownSprites[0];
-                _swordsImg.sprite = _cooldownSprites[2];
-                _heartsBackImg.sprite = _cooldownSprites[4];
-                _swordsBackImg.sprite = _cooldownSprites[4];
-            }
-        }
+		public Sprite GetSprite(bool state)
+		{
+			var changeSpriteScript = avatarGameObject.GetComponent<UISpriteImageStateChange>();
+			var sprite = state ? changeSpriteScript.GetTrueStateSprite : changeSpriteScript.GetFalseStateSprite;
+			return sprite;
+		}
 
-        private void SetupStat(TMP_Text text, string value)
-        {
-            text.text = value;
-        }
+		public void ChangeAvatarState(bool state)
+		{
+			var changeSpriteScript = avatarGameObject.GetComponent<UISpriteImageStateChange>();
+			if(state)
+				changeSpriteScript.EnableState();
+			else
+				changeSpriteScript.DisableState();
+		}
 
-        public void SetUpAllStats(string attackValue, string healthValue, bool isShielded)
-        {
-            SetupStat(attackText, attackValue);
-            SetupStat(healthText, healthValue);
-            SetShield(isShielded);
-            ChangeShieldSprite();
-        }
+		public void SetupSpeed(int speed)
+		{
+			DeleteAllSpeedObject();
+			for(var i = 0; i < speed; i++)
+			{
+				var speedObject = Instantiate(speedPrefab, speedArea.transform);
+				createdSpeedObjectList.Add(speedObject);
+			}
 
-        private void SetShield(bool value)
-        {
-            _isShielded = value;
-            ChangeShieldSprite();
-        }
+			var speedObjectBonus = Instantiate(speedPrefabBonus, speedArea.transform);
+			createdSpeedObjectList.Add(speedObjectBonus);
+			speedObjectBonus.SetActive(false);
 
-        private void ChangeShieldSprite()
-        {
-            shieldSprite.enabled = _isShielded;
-        }
+			if(SceneManager.GetActiveScene().name == "ArcadeGameLoop")
+			{
+				speedArea.GetComponent<SpeedPanelAnimationHelper>().FindSpeedBallAnimationHelper();
+			}
+		}
 
-        public void SetupAvatar(Sprite enableState, Sprite disableState)
-        {
-            var changeSpriteScript = avatarGameObject.GetComponent<UISpriteImageStateChange>();
-            changeSpriteScript.SetupSprites(enableState, disableState);
-        }
+		private void DeleteAllSpeedObject()
+		{
+			foreach (var speedObject in createdSpeedObjectList)
+			{
+				Destroy(speedObject);
+			}
 
-        public Sprite GetSprite(bool state)
-        {
-            var changeSpriteScript = avatarGameObject.GetComponent<UISpriteImageStateChange>();
-            var sprite = state ? changeSpriteScript.GetTrueStateSprite : changeSpriteScript.GetFalseStateSprite;
-            return sprite;
-        }
+			createdSpeedObjectList.Clear();
+		}
 
-        public void ChangeAvatarState(bool state)
-        {
-            var changeSpriteScript = avatarGameObject.GetComponent<UISpriteImageStateChange>();
-            if (state)
-                changeSpriteScript.EnableState();
-            else
-                changeSpriteScript.DisableState();
-        }
+		public void EnabledBonusSpeedSrite()
+		{
+			createdSpeedObjectList.Last().SetActive(true);
+			if(animToTrigger != null) animToTrigger.onRerollTrigger -= EndRerollAnimation;
+			DeleteAllSpeedObject();
+		}
 
-        public void SetupSpeed(int speed)
-        {
-            DeleteAllSpeedObject();
-            for (var i = 0; i < speed; i++)
-            {
-                var speedObject = Instantiate(speedPrefab, speedArea.transform);
-                createdSpeedObjectList.Add(speedObject);
-            }
-            var speedObjectBonus = Instantiate(speedPrefabBonus, speedArea.transform);
-            createdSpeedObjectList.Add(speedObjectBonus);
-            speedObjectBonus.SetActive(false);
+		private void OnDisable()
+		{
+			DeleteAllSpeedObject();
+		}
 
-            if (SceneManager.GetActiveScene().name == "ArcadeGameLoop")
-            {
-                speedArea.GetComponent<SpeedPanelAnimationHelper>().FindSpeedBallAnimationHelper();
-            }
-        }
-
-        private void DeleteAllSpeedObject()
-        {
-            foreach (var speedObject in createdSpeedObjectList)
-            {
-                Destroy(speedObject);
-            }
-
-            createdSpeedObjectList.Clear();
-        }
-        public void EnabledBonusSpeedSrite()
-        {
-            createdSpeedObjectList.Last().SetActive(true);
-            if (animToTrigger != null) animToTrigger.onRerollTrigger -= EndRerollAnimation;
-            DeleteAllSpeedObject();
-        }
-
-    }
+	}
 }
--- BattleCombine/Assets/_Scripts/UI/UIObjects/UISpriteImageStateChange.cs
+++ BattleCombine/Assets/_Scripts/UI/UIObjects/UISpriteImageStateChange.cs
@@ -3,34 +3,60 @@ using UnityEngine.UI;
 
 namespace BattleCombine.UI
 {
-    public class UISpriteImageStateChange : MonoBehaviour
-    {
-        [SerializeField] private Sprite spriteStateTrue;
-        [SerializeField] private Sprite spriteStateFalse;
-        [SerializeField] private Image image;
+	public class UISpriteImageStateChange : MonoBehaviour
+	{
+		[SerializeField] private Sprite spriteStateTrue;
+		[SerializeField] private Sprite spriteStateFalse;
+		[SerializeField] private Image image;
+		[SerializeField] private Image avatarBackgroud;
 
-        public Sprite GetTrueStateSprite => spriteStateTrue;
-        public Sprite GetFalseStateSprite => spriteStateFalse;
-        public void SetupSprites(Sprite spriteTrue, Sprite spriteFalse)
-        {
-            spriteStateTrue = spriteTrue;
-            spriteStateFalse = spriteFalse;
-            EnableState();
-        }
+		[Header("Borders")]
+		[SerializeField] private GameObject avatarBorder;
+		[SerializeField] private GameObject statBorder;
 
-        private void Start()
-        {
-            image.sprite = spriteStateFalse;
-        }
+		public Sprite GetTrueStateSprite => spriteStateTrue;
 
-        public void EnableState()
-        {
-            image.sprite = spriteStateTrue;
-        }
+		public Sprite GetFalseStateSprite => spriteStateFalse;
 
-        public void DisableState()
-        {
-            image.sprite = spriteStateFalse;
-        }
-    }
+		public void SetupSprites(Sprite spriteTrue, Sprite spriteFalse)
+		{
+			spriteStateTrue = spriteTrue;
+			spriteStateFalse = spriteFalse;
+			EnableState();
+			ShowBorders(false);
+		}
+
+		private void Start()
+		{
+			DisableState();
+		}
+
+		private void ShowBorders(bool show)
+		{
+			if(avatarBorder == null && statBorder == null) return;
+
+			avatarBorder.SetActive(show);
+			statBorder.SetActive(show);
+		}
+
+		public void EnableState()
+		{
+			image.sprite = spriteStateTrue;
+
+			ShowBorders(true);
+
+			if(avatarBackgroud == null) return;
+			avatarBackgroud.color = new Color(1, 1, 1, 1);
+		}
+
+		public void DisableState()
+		{
+			image.sprite = spriteStateFalse;
+
+			ShowBorders(false);
+			
+			if(avatarBackgroud == null) return;
+			avatarBackgroud.color = new Color(1, 1, 1, 0.5f);
+		}
+	}
 }
